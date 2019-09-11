48353ede5d2f568f84c2de892cd8db3bbaef42e9
sinteary <leewengyancynthia@gmail.com>
2019-09-11 15:50:54 +0800

    Add JUnit tests for ToDo class

---
+++ src/test/java/ToDoTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import tasks.ToDo;
+
+public class ToDoTest {
+
+  @Test
+  public void toString_success () {
+    ToDo todo1 = new ToDo("Finish homework");
+    assertEquals("[T][\u2718] Finish homework", todo1.toString());
+    ToDo todo2 = new ToDo("Submit appeal");
+    assertEquals("[T][\u2718] Submit appeal", todo2.toString());
+  }
+
+  @Test
+  public void getTaskName_success() {
+    ToDo todo1 = new ToDo("Finish homework");
+    assertEquals("Finish homework", todo1.getTaskName());
+    ToDo todo2 = new ToDo("Submit appeal");
+    assertEquals("Submit appeal", todo2.getTaskName());
+  }
+
+}
